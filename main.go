39e6fd63180a1f22bede6bf6f963b6bf86ca308a
jason <lvs.pjx@gmail.com>
2021-02-04 11:16:06 +0800

    *: remove fyne

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"strings"
 
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/layout"
-	"fyne.io/fyne/widget"
-
 	"main/options"
-	"main/pkg/logger"
 	"main/server"
 
 	"github.com/flopp/go-findfont"
@@ -30,40 +26,17 @@ func init() {
 }
 
 func main() {
-	myApp := app.New()
-	myWin := myApp.NewWindow("配置信息")
+	b, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	o := &options.Option{}
+	json.Unmarshal(b, &o)
 
-	serverConfig := widget.NewEntry()
-	serverConfig.SetPlaceHolder("192.168.0.10:2000")
+	if o.Client == "" {
+		o.Client = "192.168.0.10:2000"
+	}
 
-	uri := widget.NewEntry()
-	uri.SetPlaceHolder("username")
-
-	port := widget.NewEntry()
-	port.SetPlaceHolder(":2001")
-
-	serverBox := widget.NewHBox(widget.NewLabel("PLC服务地址:"), layout.NewSpacer(), serverConfig)
-	URIBox := widget.NewHBox(widget.NewLabel("数据库资源定位符:"), layout.NewSpacer(), uri)
-	portBox := widget.NewHBox(widget.NewLabel("上位机端口:"), layout.NewSpacer(), port)
-
-	startBtn := widget.NewButton("Run", func() {
-		o := &options.Option{
-			Client:   serverConfig.Text,
-			Username: uri.Text,
-			Server:   port.Text,
-		}
-
-		if o.Client == "" {
-			o.Client = "192.168.0.10:2000"
-		}
-
-		fmt.Println("server endpoint:", serverConfig.Text, "uri:", uri.Text, "port: ", port.Text, "start")
-		s := server.NewServer(o)
-		go s.Run()
-	})
-
-	content := widget.NewVBox(serverBox, URIBox, portBox, startBtn)
-	myWin.SetContent(content)
-	logger.Printf("start show and run")
-	myWin.ShowAndRun()
+	s := server.NewServer(o)
+	s.Run()
 }
